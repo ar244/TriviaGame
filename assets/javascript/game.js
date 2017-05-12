34f1c53ae87b01285b9dbae3da9fc345f592936a
ar244 <anu244@gmail.com>
2017-05-12 00:29:20 -0500

    temp

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,19 +5,29 @@ $(document).ready(function(){
 	var wrong = 0;
 	var incomplete = 0;
 	var qNumber = 0;
+	var i=0;
+
+	var intervalId;
+	var count;
+	var count1;
+   var running = false;
+
 
 	var quiz = [ 
 			["Which city has the famous Big Ben?",
 			  "Paris", "London", "Bangkok", "Chicago",
-			  "London"
+			  "London",
+			  "assets/images/BigBen.gif"
 			],
 			["Which European city is known as the City of Lights?",
 			 "Chicago", "Beijing", "Paris", "Sydney",
-			 "Paris"
+			 "Paris",
+			 "assets/images/Paris.gif"
 			 ],
 			["New Delhi is the capital of which country?",
 			 "India", "Brazil", "Egypt", "Indonesia",
-			 "India"
+			 "India",
+			 "assets/images/Delhi.gif"
 			]
 	];
 
@@ -30,75 +40,142 @@ $(document).ready(function(){
 
 
 	var createTimer = function() {
+		count = 10;
 		$("#mainBox").empty();
 		var time = document.createElement("div");
-		$(time).html("<div class='text-center' id='timer'>Time left: 30</div>");
+		$(time).html("<div class='text-center' id='timer'>Time left: " + count +"</div>");
 		$("#mainBox").append(time);
-
+		intervalId = setInterval(function () {
+			count--; 
+			if (count === 0) {
+				i++;
+				incomplete++;
+				clearInterval(intervalId);
+				nextQ();	
+			}
+			$(time).html("<div class='text-center' id='timer'>Time left: " + count +"</div>");
+			}, 1000);
 	}
 
-	var displayQuiz = function() {
-		var br = document.createElement("br");
-		$(br).html("<br>");
-		for (var i=0; i<quiz.length;i++) {
-			
-			var questionBox = document.createElement("div");
-			$(questionBox).html("<div class='text-center' id='question'>"+ quiz[i][0]+"</div>");
-			$("#mainBox").append(questionBox);
-			$("#mainBox").append(br);
+	
 
+
+	var displayQuiz = function(quiz, i) {
+		if (i<= quiz.length) {
 			
-			var ans = document.createElement("form");
-			for (var j=1; j<5; j++) {
-				var radioId = "ans" + i;
-				//$(ans).append("<input id=" + radioId + "type='radio' name='answer' value =" + quiz[i][j] +">" + quiz[i][j]);
-			  $(ans).append("<input type='radio' name='answer' value =" + quiz[i][j] +">" + quiz[i][j]);
-			}
-			$("#mainBox").append(ans);
-			$("#mainBox").append(br);
+
+			// empty div for questions
+			var quizBox = document.createElement("div");
+			$(quizBox).attr("id", "quizBox");
+			$("#mainBox").append(quizBox);
+
+			//$("#quizBox").empty();
+			
+				
+				var questionBox = document.createElement("div");
+				$(questionBox).html("<div class='text-center' id='question'>"+ quiz[i][0]+"</div>");
+				$("#quizBox").append(questionBox);
+
+
+				for (var j=1; j<5; j++) { 
+					var ansB = $("<button>");
+					ansB.text(quiz[i][j]);
+					ansB.addClass("button ansChoice");
+					ansB.attr("data-name", quiz[i][j]);
+					$("#quizBox").append(ansB);	
+				}	
+		}
+	}
+
+
+	$(document).on("click", ".ansChoice", function() {
 		
+		var ans = $(this).attr("data-name");
+		console.log(ans);
+		clearInterval(intervalId);  // stop timer
 
+		$("#quizBox").empty();
+
+		var tmp = $("<div>");
+		tmp.attr("id", "tmpAns");
+		var gif = $("<img>");
+		gif.attr("id", "gif");
+		gif.attr("src", quiz[i][6]);
+
+		if (ans === quiz[i][5]) {
+			correct++;
+			
+			tmp.text("Correct!!!");
+			
+			$("#quizBox").append(tmp);
+			$("#quizBox").append(gif);
+			setTimeout(function() {
+        		console.log("here");
+        		//$("#quizBox").empty();
+    		}, 5000); 
+
+
+		} else {
+
+			wrong++;
 		}	
 		
-	}
+		// show answer and gif
 
-	var displayDoneButton = function () {
-		var doneB = document.createElement("button");
-		$(doneB).text("Done");
-		$(doneB).attr("id", "done");
-		$("#mainBox").append(doneB);
-	}
-
-
-	
-
-	$(document).on("click", "#done", function() {
-		
-		var ans = document.getElementsByName("answer");
-		console.log(ans);
-		for (var x=0; x<ans.length; x++) {
-			var tmp = (x%4)-1;   // to get question number since there are 4 buttons per question
-			if (ans[x].checked) {
-				console.log("value = " + ans[x].value);
-				if (ans[x] === quiz[tmp][5]) {
-					correct++;
-				} else {
-					wrong++;
-				}
-			} else {
-				incomplete++;
-			}
-		}
-
-		console.log(correct); console.log(wrong); console.log(incomplete);
-
-	
+		i++;
+		nextQ();	
 	});
 
+	
+
+	
+	var showResult = function() {
+		console.log(correct);
+		console.log(wrong);
+		console.log(incomplete);
+		$("#mainBox").empty();
+		var done = $("<img>");
+		done.attr("id", "done");
+		done.attr("src", "assets/images/done.jpg");
+
+		var rBox = $("<div>");
+		rBox.attr("id", "rBox");
+
+		var c = $("<div>");
+		c.attr("id", "correct");
+		c.text("Correct answers: " + correct);
+
+		var w = $("<div>");
+		w.attr("id", "wrong");
+		w.text("Wrong answers: " + wrong);
+
+		var i = $("<div>");
+		i.attr("id", "incomplete");
+		i.text("Incomplete answers: " + incomplete);
+
+		// append above
+		$("#mainBox").append(done);
+		$("#mainBox").append(rBox);
+		$("#rBox").append(c);
+			$("#rBox").append(w);
+				$("#rBox").append(i);
+
+	}
+
+	// Start the quiz
 	$("#start").click(function() {
-		createTimer();
-		displayQuiz();
-		displayDoneButton();
+		i=0;
+		nextQ();
 	});
+
+	var nextQ = function() {
+		if (i === quiz.length) {
+			showResult();
+		} else {
+			createTimer();
+			displayQuiz(quiz, i);
+
+		}
+	}
 
 });
